0dce34d9087ebb7062daf2f78c9d67403ea78f0b
Anatoly Korniltsev <korniltsev.anatoly@gmail.com>
2015-05-20 23:01:43 +0300

    delete unused libs

--- libs/libopus/src/obj/local/armeabi/objs/opus2/celt/arm/armcpu.o.d
+++
@@ -1,2 +0,0 @@
-/Users/korniltsev/IdeaProjects/client2/libs/libopus/src/obj/local/armeabi/objs/opus2/./celt/arm/armcpu.o: \
- /Users/korniltsev/IdeaProjects/client2/libs/libopus/src/jni/opus/./celt/arm/armcpu.c
--- libs/libopus/src/obj/local/x86/objs/opus/celt/arm/armcpu.o.d
+++
@@ -1,2 +0,0 @@
-/Users/korniltsev/IdeaProjects/client2/libs/libopus/src/obj/local/x86/objs/opus/./celt/arm/armcpu.o: \
- /Users/korniltsev/IdeaProjects/client2/libs/libopus/src/jni/opus/./celt/arm/armcpu.c
