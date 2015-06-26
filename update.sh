a0d4091c088b638110277cf01aa6e6c9b32b792f
Anatoly Korniltsev <korniltsev.anatoly@gmail.com>
2015-06-26 14:45:25 +0300

    update script

--- update.sh
+++ update.sh
@@ -1,35 +1,14 @@
-#!/usr/local/bin/groovy
+#!/bin/bash
 
-
-def static process(File file, Closure processText) {
-    def text = file.text
-    String newText = processText(text)
-    //println newText
-    file.write(newText)
-}
-def VERSION_NAME_PATTERN = /versionName\s+\"(.*)\"/
-def VERSION_CODE_PATTERN = /versionCode\s+(\d+)/
-def file = new File("main_lib/build.gradle")
-
-process(file) {	        
-	(it =~ VERSION_NAME_PATTERN).with {
-		def (major,minor,fix) = getAt(0)[1].split("\\.").collect { it as int }			
-		println (++fix)
-		replaceAll("versionName \"${major}.${minor}.${fix}\"")	                    
-	}
-}
-
-process(file){
-	(it =~ VERSION_CODE_PATTERN).with {
-		def versionCode = getAt(0)[1] as int
-		println (++versionCode)
-		
-		replaceAll("versionCode ${versionCode}")	                    
-	}
-}
-
-"git add .".execute().waitFor()
-"git ci -m bump_version".execute().waitFor()
+#increase verion
+perl -pe 's/(versionCode )(\d+)/$1.($2+1)/e' < main_lib/build.gradle > tmp
+perl -pe 's/(versionName \"\d+\.\d+\.)(\d+)(\")/$1.($2+1).$3/e' < tmp > main_lib/build.gradle
+rm tmp
+#git
+git add .
+git ci -m "bump version"
+#publish
+./gradlew publishRelease
 
 
 
